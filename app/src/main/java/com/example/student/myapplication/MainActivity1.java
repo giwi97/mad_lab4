29b816c5bd45306c261537727cb1266c0629f023
giwi97 <49786113+giwi97@users.noreply.github.com>
2019-07-30 12:21:39 +0530

    Add a commit

--- app/src/main/java/com/example/student/myapplication/MainActivity1.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity1.java
@@ -40,7 +40,7 @@ public class MainActivity1 extends AppCompatActivity {
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(layout);
         toast.show();
-
+//I am from github
     }
 
 }
